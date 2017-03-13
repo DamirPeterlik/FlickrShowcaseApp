7f688c0464ecc0253984bb241d47fac23f67e2a5
Damir Peterlik <damir@martian.agency>
2017-03-13 20:31:42 +0100

    cleaning folders

--- FlickrShowcaseApp/FlickrPhotoDetails/Modules/FlickrPhotoDetailsWireframe.swift
+++
@@ -1,9 +0,0 @@
-//
-//  FlickrPhotoDetailsWireframe.swift
-//  FlickrShowcaseApp
-//
-//  Created by Damir Peterlik on 09/03/2017.
-//  Copyright © 2017 Damir Peterlik. All rights reserved.
-//
-
-import Foundation
