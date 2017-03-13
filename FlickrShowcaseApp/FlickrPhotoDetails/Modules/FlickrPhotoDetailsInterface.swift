34cdbe6d413c7c73ba45237f71067a3b7ebdbae1
Damir Peterlik <damir@martian.agency>
2017-03-13 19:27:27 +0100

    First commit of project late stage

--- FlickrShowcaseApp/FlickrPhotoDetails/Modules/FlickrPhotoDetailsInterface.swift
+++ FlickrShowcaseApp/FlickrPhotoDetails/Modules/FlickrPhotoDetailsInterface.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2017 Damir Peterlik. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class FlickrPhotoDetailsInterface {
+    
+    static let sharedInstance = FlickrPhotoDetailsInterface()
+    
+    func configure(_ viewController: FlickrPhotoDetailsViewController) {
+        let dataManager: FlickrDataManagerPhotoLoadProtocol = FlickrDataManager()
+        let interactor = FlickrPhotoDetailsInteractor()
+        let presenter = FlickrPhotoDetailsPresenter()
+        viewController.presenter = presenter
+        presenter.view = viewController
+        interactor.presenter = presenter
+        presenter.interactor = interactor
+        interactor.dataManager = dataManager
+    }
+}
---
+++ Pods/Target Support Files/Pods-FlickrShowcaseApp/Pods-FlickrShowcaseApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlickrShowcaseApp : NSObject
+@end
+@implementation PodsDummy_Pods_FlickrShowcaseApp
+@end
